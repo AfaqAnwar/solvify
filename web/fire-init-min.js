edfd4c1068430f786fb37acc2751d3587558dbce
Afaq Anwar <anwarafaq@protonmail.com>
2023-09-12 16:25:49 -0400

    OBFUSCATE

--- web/fire-init-min.js
+++ web/fire-init-min.js
@@ -1,9 +1,1 @@
-import { initializeApp } from "./firebase/firebase-app.js";
-window.appInstance = initializeApp({
-  apiKey: "AIzaSyApPgB8BVZ3E68AAMR2LvdQj61iqmxlt2Y",
-  authDomain: "solvify-chrome-extension.firebaseapp.com",
-  projectId: "solvify-chrome-extension",
-  storageBucket: "solvify-chrome-extension.appspot.com",
-  messagingSenderId: "3979832668",
-  appId: "1:3979832668:web:d58c0084dddb5403e2e599",
-});
+import{initializeApp}from"./firebase/firebase-app.js";window.appInstance=initializeApp({apiKey:"AIzaSyApPgB8BVZ3E68AAMR2LvdQj61iqmxlt2Y",authDomain:"solvify-chrome-extension.firebaseapp.com",projectId:"solvify-chrome-extension",storageBucket:"solvify-chrome-extension.appspot.com",messagingSenderId:"3979832668",appId:"1:3979832668:web:d58c0084dddb5403e2e599"});
